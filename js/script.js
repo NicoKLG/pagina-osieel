1b6f291c3c338daff5769fae2a9063efcbb0e219
NicoKLG <lopezkendra040703@gmail.com>
2024-12-11 17:46:11 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+    let lastScrollTop = 0;
+    const navbar = document.querySelector('.navbar');
+
+    window.addEventListener('scroll', function() {
+        let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        if (scrollTop > lastScrollTop) {
+            navbar.classList.add('hidden');
+        } else {
+            navbar.classList.remove('hidden');
+        }
+        lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For Mobile or negative scrolling
+    });
